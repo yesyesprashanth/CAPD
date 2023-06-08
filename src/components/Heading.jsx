7b11b411dde490d9bcbf86af931b87fd843b1dd6
Prashanth <yesyesprashanth@gmail.com>
2023-06-08 14:28:02 +0530

    first upload

---
+++ src/components/Heading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Heading(){
+    return(
+        <>
+            <h1>Auditory and Auditory-Cognitive Training Program</h1>
+        </>
+    )
+}
